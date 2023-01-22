460e66d26d81cc49245a8136321574ad069f43d4
Joel Ramirez Garay <113269346+EnriqueR20@users.noreply.github.com>
2023-01-22 17:40:51 -0500

    Creacion de Rutas
    Para poder expórtar las rutas

---
+++ source/components/index.ts
@@ -0,0 +1,6 @@
+
+/* Acceso para las capertas*/
+
+export { default as usuarioRouter } from "./usuario";
+export { default as musicaRouter } from "./musica";
+export { default as playlistRuta } from "./playlist";
